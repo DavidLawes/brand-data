923958c9a5251bd3f4013221b9687c00df69e7cd
David Lawes <davidlawes@hotmail.co.uk>
2025-02-15 17:51:18 +0000

    update product service to return all product entities for brand

--- src/services/product/index.test.ts
+++ src/services/product/index.test.ts
@@ -1,50 +1,104 @@
-import { Product } from ".";
+import { ProductService } from ".";
 import { BrandNotFoundError } from "../../errors";
 import { Brands } from "../../models/brands";
-import { Brand } from "../../models/brands/brand.type";
+import { Products } from "../../models/products";
+
+jest.mock("../../models/brands");
+jest.mock("../../models/products");
 
 describe("Product service", () => {
+  let productService: ProductService;
+  let mockBrands: jest.Mocked<Brands>;
+  let mockProducts: jest.Mocked<Products>;
+
+  beforeEach(() => {
+    mockBrands = new (<new () => Brands>(
+      (<unknown>Brands)
+    ))() as jest.Mocked<Brands>;
+
+    mockProducts = new (<new () => Products>(
+      (<unknown>Products)
+    ))() as jest.Mocked<Products>;
+
+    productService = new ProductService(mockBrands, mockProducts);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe("Get all product entities for a brand", () => {
     describe("Given the brand cannot be found", () => {
-      const brands = new Brands([]);
-      const product = new Product(brands);
+      beforeEach(() => {
+        mockBrands.get.mockImplementation(() => {
+          throw new BrandNotFoundError("123");
+        });
+      });
 
       it("should throw a BrandNotFoundError", () => {
-        expect(() => product.getProductEntitiesByBrand("123")).toThrow(
+        expect(() => productService.getProductEntitiesByBrand("123")).toThrow(
           BrandNotFoundError,
         );
       });
     });
 
     describe("Given the brand exists", () => {
-      const brand: Brand = {
+      const mockBrand = {
         id: "123",
         created_at: "2019-03-18 18:47:05",
         updated_at: "2019-03-18 18:47:05",
         name: "independent coffee locations",
-        products: ["26f7a82a-30a8-44e4-93cb-499a256d0ce9"],
+        products: ["prod123"],
         consolidated_products: [],
       };
-
-      const brands = new Brands([brand]);
-      const product = new Product(brands);
+      const mockProduct = { id: "prod123" };
+      beforeEach(() => {
+        mockBrands.get.mockReturnValue(mockBrand);
+        mockProducts.findMany.mockReturnValue([mockProduct]);
+      });
 
       it("should return all product entities for given brand", () => {
-        expect(product.getProductEntitiesByBrand("123")).toEqual(
-          brand.products,
-        );
+        expect(productService.getProductEntitiesByBrand("123")).toEqual([
+          mockProduct,
+        ]);
+
+        expect(mockBrands.get).toHaveBeenCalledWith("123");
+        expect(mockProducts.findMany).toHaveBeenCalledWith(mockBrand.products);
       });
 
       describe("Given the brand has no products", () => {
-        const brands = new Brands([
-          {
-            id: "123",
-          },
-        ]);
-        const product = new Product(brands);
+        beforeEach(() => {
+          mockBrands.get.mockReturnValue(mockBrand);
+          mockProducts.findMany.mockReturnValue([]);
+        });
 
         it("should return an empty array", () => {
-          expect(product.getProductEntitiesByBrand("123")).toEqual([]);
+          expect(productService.getProductEntitiesByBrand("123")).toEqual([]);
+        });
+      });
+
+      describe("Given the brand has consolidated products", () => {
+        const mockBrandWithConsolidatedProducts = {
+          id: "123",
+          products: ["prod123"],
+          consolidated_products: ["con456"],
+        };
+
+        beforeEach(() => {
+          mockBrands.get.mockReturnValue(mockBrandWithConsolidatedProducts);
+          mockProducts.findMany.mockReturnValue([
+            { id: "prod123" },
+            { id: "con456" },
+          ]);
+        });
+        it("should return all products", () => {
+          productService.getProductEntitiesByBrand("123");
+
+          expect(mockBrands.get).toHaveBeenCalledWith("123");
+          expect(mockProducts.findMany).toHaveBeenCalledWith([
+            "prod123",
+            "con456",
+          ]);
         });
       });
     });
--- src/services/product/index.ts
+++ src/services/product/index.ts
@@ -1,20 +1,28 @@
 import { BrandNotFoundError } from "../../errors";
 import { Brands } from "../../models/brands";
+import { Products } from "../../models/products";
+import { Product } from "../../models/products/product.type";
 
-export class Product {
+export class ProductService {
   brands: Brands;
+  products: Products;
 
-  constructor(brands: Brands) {
+  constructor(brands: Brands, products: Products) {
     this.brands = brands;
+    this.products = products;
   }
 
-  getProductEntitiesByBrand(brandId: string): string[] {
+  getProductEntitiesByBrand(brandId: string): Product[] {
     const brand = this.brands.get(brandId);
 
     if (!brand) {
       throw new BrandNotFoundError(brandId);
     }
 
-    return brand.products || [];
+    const productsForBrand: string[] = (brand.products || []).concat(
+      brand.consolidated_products,
+    );
+
+    return this.products.findMany(productsForBrand);
   }
 }
